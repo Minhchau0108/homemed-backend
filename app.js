a506852497ba97622c4759bce13159ecc705e11a
MinhChau <minhchau3315@gmail.com>
2021-03-29 14:13:23 +0700

    change

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, "public")));
 /* DB Connections */
 mongoose
   .connect(MONGODB_URI, {
